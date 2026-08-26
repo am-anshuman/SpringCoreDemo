1f59ae1a5a7efbd694064f5340462f2d2ce2b730
am-anshuman <anshumansahu0310@gmail.com>
2026-08-26 08:59:10 +0530

    package path changes for ComponentScan

--- src/main/java/org/aman/ioc_container/AppConfig.java
+++ src/main/java/org/aman/ioc_container/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration // signifies that this is a configuration class
-@ComponentScan("org.aman") // performs component scan in the specified package
+@ComponentScan("org.aman.ioc_container") // performs component scan in the specified package
 public class AppConfig {
 
     @Bean
